54883d92ec4b925ef6da53d7b953ad94bb25c8b8
mjaelo <44395343+mjaelo@users.noreply.github.com>
2020-09-07 22:06:17 +0200

    added color change

--- sledzenie.py
+++ sledzenie.py
@@ -1,12 +1,17 @@
 import cv2
 import os
 
+#uwagi:
+# interfejs graficzny powinien miec: pola do zmiany wartosci parametrow, guzik on/off gumki, guzik wyjscie
+
+# mogę zmienic funkcje szukania koloru, by  miała tablice rozmiaru obrazka i kolorowanie by zapelnialo pola tego obrazka, oszczedzilo by to pamiec,
+# ale i tak usuwam najstarsze pola, a tak, to bym dal jakis klor nieuzywany, by oznaczyc pola puste. Wtedy te nie byloby usuwania starych pol
 
 class Parameters:
-    value = [255, 255, 255]
+    value = [255, 255, 255] # szukany kolor
     thickness = 10
     search_area = [100, 100]
-    draw_color = value # kolor rysowania
+    draw_color = [255, 255, 255] # kolor rysowania
     max_table_size = 100 #dlugosc ile pikseli naraz bedzie pokolorowanych
 
 # dziala, prostrze, ale muli, bo patrzy na caly obrazek
@@ -75,7 +80,6 @@ def findclosestpix_part(value, img, center_xy, area):
                     min_xy = [pozX, pozY]
         errors = 0
     except:
-        ok = "ok"
         errors += 1
         print("["+str(pozX)+","+str(pozY)+"] searching error nr." + str(errors))
         if errors > 5:
@@ -84,8 +88,9 @@ def findclosestpix_part(value, img, center_xy, area):
             print("fixed")
     return min_xy
 
+#organizowanie znalezionych pikseli do pokolorowania
 def searchcam(param):
-    temporary = "ok"
+    is_on = True
     max_xy_tab = []
 
     cap = cv2.VideoCapture(0)
@@ -93,9 +98,9 @@ def searchcam(param):
     print("wymiary obrazu: ", len(img), len(img[0]))
 
     latest_match = findclosestpix_all(param.value, img)
-    max_xy_tab.append(latest_match)  # znalezienie najblizszego matchu na calym obrazie w pierwszej klatce
+    max_xy_tab.append([latest_match,param.draw_color])  # znalezienie najblizszego matchu na calym obrazie w pierwszej klatce
 
-    while (temporary == "ok"):
+    while (is_on == True):
         # zczytanie klatek kamery do obrazu
         ret, img = cap.read()
         key = cv2.waitKey(30)
@@ -105,31 +110,34 @@ def searchcam(param):
         latest_match = [(latest_match[0] + latest_match2[0]) / 2,
                         (latest_match[1] + latest_match2[
                             1]) / 2]  # strednia starego i nowego punktu, by ograniczyc wariactwa
-        max_xy_tab.append(latest_match)
+        max_xy_tab.append([latest_match,param.draw_color])
         if len(max_xy_tab) > param.max_table_size:
             max_xy_tab.remove(max_xy_tab[0])
+
 
         # rysowanie w zapisanych lokacjach
         if param.thickness > 1:
             for point in max_xy_tab:
                 for i in range(param.thickness):
                     for j in range(param.thickness):
-                        if 0 < point[0] + i < len(img) and 0 < point[1] + i < len(
+                        if 0 < point[0][0] + i < len(img) and 0 < point[0][1] + i < len(
                                 img[1]):
                             try:
+                                # obecny punkt jako przeciwienstwo zwyklego koloru
                                 if (point == max_xy_tab[len(max_xy_tab) - 1]):
-                                    # obecny punkt jako przeciwienstwo zwyklego koloru
-                                    img[int(point[0]) + i][int(point[1]) + j] = [255 - param.draw_color[0],
+
+                                    img[int(point[0][0]) + i][int(point[0][1]) + j] = [255 - param.draw_color[0],
                                                                                  255 - param.draw_color[1],
                                                                                  255 - param.draw_color[2]]
                                 else:
-                                    img[int(point[0]) + i][int(point[1]) + j] = param.draw_color
+                                    img[int(point[0][0]) + i][int(point[0][1]) + j] = point[1]
                             except:
                                 print("coloring error")
                                 ok = "ok"
         cv2.imshow('teraz', img)
     cap.release()
     cv2.destroyAllWindows()
+
 
 
 if __name__ == "__main__":
